c8f3fcf8943f074aba921f523bb6c89a29296700
Zeyuan Hu <zeyuan_hu@fas.harvard.edu>
2020-11-17 07:28:16 +0800

    change der method name to forward and fixed a bug

--- farad/driver.py
+++ farad/driver.py
@@ -1,4 +1,6 @@
 from farad.dual import Dual
+from numbers import Number
+
 
 class AutoDiff:
 
@@ -7,29 +9,29 @@ class AutoDiff:
 
     def get_val_scalar(self, val):
         # for a number, evaluated at a single variable.
-        if isinstance(val, number):
-            a = Dual(val,1)
+        if isinstance(val, Number):
+            a = Dual(val, 1)
             return self.fn(a).val
         # for a list of numbers, evaluated at a single variable.
         elif isinstance(val, list):
             vals = []
             for v in val:
-                a = Dual(v,1)
+                a = Dual(v, 1)
                 vals.append(self.fn(a).val)
             return vals
         else:
             raise Exception("Currently we have not accepted input types other than number and list")
 
-    def get_der_scalar(self, val):
+    def forward(self, val):
         # for a number, get the derivative at a single variable
-        if isinstance(val, number):
-            a = Dual(val,1)
+        if isinstance(val, Number):
+            a = Dual(val, 1)
             return self.fn(a).der
         # for a list of numbers, get the derivative at a single variable
         elif isinstance(val, list):
             ders = []
             for v in val:
-                a = Dual(v,1)
+                a = Dual(v, 1)
                 ders.append(self.fn(a).der)
                 return ders
         else:
