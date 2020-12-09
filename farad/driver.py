97d8906b6dd83f3303ddcea5121af310fb47eb6a
Matthew Stewart <matthew_stewart@g.harvard.edu>
2020-12-08 22:59:41 -0500

    Added multivariate and vector input function capabilities to driver.py.

--- farad/driver.py
+++ farad/driver.py
@@ -1,4 +1,4 @@
-"""This is the driver for Project Farad.
+"""This is the driver for Farad.
 
 This file contains class AutoDiff which has two methods.
 get_var_scalar() is for getting scalar variable including a number or a list.
@@ -10,68 +10,182 @@ Note that for milestone2 this class only deals with scalar, the function will be
 
 from farad.dual import Dual
 from numbers import Number
+from inspect import signature
 from farad.elem import *
 
 
-class AutoDiff:
+class AutoDiff(object):
 
-    def __init__(self, fn):
+
+    def __init__(self, function, dim=1):
         """Constructor for AutoDiff class.
 
         Parameters
         ==========
-        fn: The specific AD method for calculating the derivative.
-
+        function: The specific AD method for calculating the derivative.
+        dim: The dimensionality of the function.
+        
         Notes
         =====
         Currently, only first order derivatives of scalar functions via
         forward mode are supported. The methods and the range of functions
         will be extended in later versions to handle vector inputs with Jacobians.
         """
-        self.fn = fn
+        self.function = function  # function input (e.g., lambda x, y: x**2 + y**2)
+        self.dimensions = dim  # dimensionality of function input (e.g., 2 for lambda x: [x**2, x**3])
+        self._vals = []
+        self._ders = []
+        try:
+            # if defined as lambda function
+            self.length = len(self.function.__code__.co_varnames)  # no. of function inputs (e.g., 2 for lambda x, y: x**2 +  y**2)
+        except AttributeError:  # if defined as standard function
+            self.length = signature(self.function)
 
-    def get_val_scalar(self, val):
-        """Get a scalar or a list of scalars.
 
-        Args:
-            val: a scalar or a list of scalars.
+    def values(self, val):
+        """Returns node values after propagation through the computational graph.
 
-        Returns: self.fn(a).val or list of self.fn(a).val.
+        Parameters
+        ==========
+        val: a float/integer scalar or a list of scalars.
+            Values to be propagated from the input node through the computational graph.
 
+        Returns
+        =======
+        list[float|int], float, or int.
+            Returns list of float or integer values corresponding to output node values.
         """
-        # evaluate a number at a single variable.
-        if isinstance(val, Number):
-            a = Dual(val, 1)
-            return self.fn(a).val
-        # evaluate a list of numbers at a single variable.
-        elif isinstance(val, list):
-            vals = []
-            for v in val:
-                a = Dual(v, 1)
-                vals.append(self.fn(a).val)
-            return vals
-        else:
-            raise Exception("Currently not support types other than number and list")
+        self._vals = []  # reset values to prevent duplicates
+
+        if self.dimensions == 1:  # (1 input -> scalar objective function)
+
+            if self.length == 1:  # (1 input parameter -> univariate)
+        
+                try:  # EAFP principle
+                    try:  # default assumption is list input
+                        for v in val:
+                            a = Dual(v, 1)
+                            self._vals.append(self.function(a).val)
+                        return self._vals
+                    except TypeError:  # defers to float/integer input
+                        a = Dual(val, 1)
+                        return self.function(a).val
+                except TypeError:
+                    raise TypeError('Only list, float, and integer inputs supported.')
+
+            else:  # (>=2 input parameters -> multivariate)
+
+                if any(isinstance(value, list) for value in val):  # parse and calculate for each list
+                    values = []
+                    for value in val:
+                        values.append(self.values(value))
+                    return values
+                try:
+                    return self.function(*val)
+                except:
+                    print(self.length)
+                    print(val)
+                    raise Exception(f'Mismatch between function parameter length: {self.length}, and input length: {len(val)}.')
+
+        else:  # (>=2 inputs -> vector objective function)
+
+            if self.length == 1:  # (1 input parameter -> univariate)
+
+                if any(isinstance(el, list) for el in val):
+                    for v in val:
+                        values = []
+                        for i in range(self.dimensions):
+                            def inner_func(*args):
+                                return self.function(*args)[i]
+                            func = AutoDiff(inner_func, dim=1)
+                            func.length = self.length
+                            values.append(func.values(v))
+                        self._vals.append(values)
+
+            else:  # (>=2 input parameters -> multivariate)
+                
+                for i in range(self.dimensions):
+                    def inner_func(*args):
+                        return self.function(*args)[i]
+                    func = AutoDiff(inner_func, dim=1)
+                    func.length = self.length
+                    values.append(func.values(v))
+            return self._vals
+
 
     def forward(self, val):
         """Forward mode method of AutoDiff class.
 
-        Args:
-            val: a scalar or a list of scalars.
+        Parameters
+        ==========
+        val: a float/integer scalar or a list of scalars.
+            Derivatives to be propagated from the input node through the computational graph.
 
-        Returns: self.fn(a).ders or list of self.fn(a).ders.
-        The derivatives of val.
+        Returns
+        =======
+        list[float|int], float, or int.
+            Returns list of float or integer values corresponding to output node derivatives.
         """
-        # Get the derivative of a number at a single variable
-        if isinstance(val, Number):
-            a = Dual(val, 1)
-            return self.fn(a).der
-        # Get the derivative of a list of numbers at a single variable
-        elif isinstance(val, list):
-            ders = []
-            for v in val:
-                a = Dual(v, 1)
-                ders.append(self.fn(a).der)
-            return ders
-        else:
-            raise Exception("Currently not support input types other than number and list")
+
+        self._ders = []   # reset values to prevent duplicates
+
+        if self.dimensions == 1:  # (1 input -> scalar objective function)
+
+            if self.length == 1:  # (1 input parameter -> univariate)
+        
+                try:  # EAFP principle
+                    try:  # default assumption is list input
+                        for v in val:
+                            a = Dual(v, 1)
+                            self._ders.append(self.function(a).der)
+                        return self._ders
+                    except TypeError:  # defers to float/integer input
+                        a = Dual(val, 1)
+                        return self.function(a).der
+                except TypeError:
+                    raise TypeError('Only list, float, and integer inputs supported.')
+
+            else:  # (>=2 input parameters -> multivariate)
+
+                if any(isinstance(value, list) for value in val):  # parse and calculate for each list
+                    values = []
+                    for value in val:
+                        values.append(self.forward(value))
+                    return values
+                try:
+                    for i in range(self.length):
+                        val2 = val.copy()
+                        val2[i] = Dual(val2[i], 1)
+                        v = self.function(*val2)
+                        if type(v) is Dual:
+                            self._ders.append(self.function(*val2).der)
+                        else:
+                            self._ders.append(0)
+                    return self._ders
+                except:
+                    raise Exception(f'Mismatch between function parameter length: {self.length}, and input length: {len(val)}.')
+
+        else:  # (>=2 inputs -> vector objective function)
+
+            if self.length == 1:  # (1 input parameter -> univariate)
+
+                if any(isinstance(el, list) for el in val):
+                    for v in val:
+                        values = []
+                        for i in range(self.dimensions):
+                            def inner_func(*args):
+                                return self.function(*args)[i]
+                            func = AutoDiff(inner_func, dim = 1)
+                            func.length = self.length
+                            values.append(func.forward(v))
+                        self._ders.append(values)
+
+            else:  # (>=2 input parameters -> multivariate)
+                
+                for i in range(self.dimensions):
+                    def inner_func(*args):
+                        return self.function(*args)[i]
+                    func = AutoDiff(inner_func, dim = 1)
+                    func.length = self.length
+                    values.append(func.forward(v))
+            return self._ders
