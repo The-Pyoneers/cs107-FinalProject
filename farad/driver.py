050cead46be8ec5ed38c7dc8b6e7401f952dd063
Zeyuan Hu <zeyuan_hu@fas.harvard.edu>
2020-12-11 08:04:28 +0800

    merge driver and driverRAD

--- farad/driver.py
+++ farad/driver.py
@@ -1,16 +1,25 @@
 """This is the driver for Farad.
 
-This file contains class AutoDiff which has two methods.
-get_var_scalar() is for getting scalar variable including a number or a list.
+This file contains class AutoDiff and RAutoDiff.
+
+AutoDiff is for forward mode， and it contains two methods：
+values() is for getting the value of the function.
 forward() is for getting the derivative of the variables via forward AD mode.
 
-Note that for milestone2 this class only deals with scalar, the function will be extended in next stage.
+RAutoDiff is for reverse mode， and it contains three methods：
+values() is for getting the value of the function.
+reverse() is for getting the derivative of the variables via reverse AD mode.
+forwardpass() is to constructor the tree structure required to perform reverse AD
+calculation. forwardpass needs to be called before using values() and reverse().
+
 """
 
 
 from farad.dual import Dual
 from numbers import Number
 from inspect import signature
+from farad.rnode import Rnode
+import numpy as np
 
 
 class AutoDiff(object):
@@ -23,7 +32,7 @@ class AutoDiff(object):
         ==========
         function: The specific AD method for calculating the derivative.
         dim: The dimensionality of the function.
-        
+
         Notes
         =====
         Currently, only first order derivatives of scalar functions via
@@ -72,7 +81,7 @@ class AutoDiff(object):
         if self.dimensions == 1:  # (1 input -> scalar objective function)
 
             if self.length == 1:  # (1 input parameter -> univariate)
-                
+
                 try:  # EAFP principle
                     try:  # default assumption is list input
                         for v in val:
@@ -112,7 +121,7 @@ class AutoDiff(object):
                     self.vals.append(values)
 
             else:
-                
+
                 self.vals = []  # reset values to prevent duplicates
                 for i in range(self.dimensions):
                     def inner_func(*args):
@@ -176,7 +185,7 @@ class AutoDiff(object):
                     return self.ders
                 except:
                     raise Exception(f'Mismatch between function parameter length: {self.length}, and input length: {len(val)}.')
-        
+
             try:  # EAFP principle
                 try:  # default assumption is list input
                     for v in val:
@@ -204,7 +213,7 @@ class AutoDiff(object):
                     self.ders.append(values)
 
             else:  # (>=2 input parameters -> multivariate)
-                
+
                 for i in range(self.dimensions):
                     def inner_func(*args):
                         return self.function(*args)[i]
@@ -212,3 +221,154 @@ class AutoDiff(object):
                     func.length = self.length
                     self.ders.append(func.forward(val))
             return self.ders
+
+
+class RAutoDiff:
+    def __init__(self, fn):
+        """Constructor for RAutoDiff class.
+
+        Parameters
+        ==========
+        fn: The specific AD method for calculating the derivative.
+        """
+        self.fn = fn
+        self._roots = None
+        self._value = None
+        self._der = None
+
+    def forwardpass(self, x):
+        """Constructor the tree structure with input X for specific AD method
+        fn. Update the value and derivative of the AD method.
+
+        Parameters
+        ==========
+        x: array_like
+
+        Returns:
+        No returns.
+
+        """
+        self._roots = None
+        self._value = None
+        self._der = None
+        x = np.asarray(x)
+        try:
+            nf = len(self.fn)  # if fn is a list of functions
+        except TypeError:
+            nf = 1
+        if nf == 1:  # only one function
+            self._value, self._der = self._forwardpass1f(x, self.fn)
+        else:  # vector functions
+            #for now, all the input functions must contain exactly the same parameters
+            #with the same order. function with only a subset of total Parameters
+            # is not allowed
+            nparams_all = [len(signature(fi).parameters) for fi in self.fn]
+            if len(set(nparams_all)) > 1:
+                raise TypeError('all input functions must contain the same parameters')
+            self._value = []
+            self._der = []
+            for idxf in range(nf):
+                tmp = self._forwardpass1f(x, self.fn[idxf])
+                self._value.append(tmp[0])
+                self._der.append(tmp[1])
+        self._value = np.asarray(self._value)
+        self._der = np.asarray(self._der)
+        if len(self._value.shape) == 2 and len(self._der.shape) == 2:
+            self._value = np.transpose(self._value, (1, 0))
+            self._der = np.transpose(self._der, (1, 0))
+        elif len(self._value.shape) == 2 and len(self._der.shape) == 3:
+            self._value = np.transpose(self._value, (1, 0))
+            self._der = np.transpose(self._der, (1, 0, 2))
+
+        try:  # if _value or _der is a scalar array (size = 1), convert array to scalar
+            if self._value.size ==1:
+                self._value = np.asscalar(self._value)
+            if self._der.size == 1:
+                self._der = np.asscalar(self._der)
+        except AttributeError:
+            pass
+
+    def _forwardpass1f(self, x, fi):  # deal with only one function case
+        """Constructor the tree structure with input X for one single AD method
+        fi. This _forwardpass1f method will be called when dealing with vector function
+        input fn = [f1, f2, f3, ...]
+
+        Parameters
+        ==========
+        x: array_like
+        fi: One AD method
+
+        Returns:
+        tmpval: array_like, the value of fi(x)
+        tmpder : array_like, the derivative of fi(x)
+
+        """
+# shoule only be called from forwardpass
+        self._roots = None
+        nparams = len(signature(fi).parameters)  # number of parameters of input function
+        if nparams == 1:  # function has only one parameter
+            if x.size == 1:  # scalar input
+                self._roots = Rnode(x)
+                f = fi(self._roots)
+                f.grad_value = 1.0
+                tmpval = f.value
+                tmpder = self._roots.grad()
+            else:  # vector input
+                if len(x.shape) > 1:
+                    raise TypeError('input dimension size not supported')
+                else:
+                    tmpval = np.zeros(len(x))
+                    tmpder = np.zeros(len(x))
+                    tmpidx = 0
+                    for xi in x:
+                        self._roots = Rnode(xi)
+                        f = fi(self._roots)
+                        f.grad_value = 1.0
+                        tmpval[tmpidx] = f.value
+                        tmpder[tmpidx] = self._roots.grad()
+                        tmpidx = tmpidx + 1
+
+        else:  # multiple input parameters (vector input)
+            if x.size == 1:
+                raise TypeError('input has insufficient parameters')
+            if len(x.shape) == 1:  # evaluate at one vector point
+                if len(x) != nparams:
+                    raise TypeError('input dimension size mismatch')
+                self._roots = [Rnode(xi) for xi in x]
+                f = fi(*self._roots)
+                f.grad_value = 1.0
+                tmpval = f.value
+                tmpder = [root.grad() for root in self._roots]
+            else:  # evaluate at multiple vector points
+                if x.shape[1] != nparams:
+                    raise TypeError('input dimension size mismatch')
+                nm = x.shape[0]  # number of vector points to be evaluated
+                tmpval = np.zeros(nm)
+                tmpder = np.zeros((nm, nparams))
+                self._roots = []
+                for im in range(nm):
+                    self._roots.append([Rnode(xi) for xi in x[im, :]])
+                    f = fi(*self._roots[im])
+                    f.grad_value = 1.0
+                    tmpval[im] = f.value
+                    tmpder[im, :] = [root.grad() for root in self._roots[im]]
+        return tmpval, tmpder
+
+    def values(self):  # return the value of the function
+        """Get value of the input method fn for given X
+
+        Returns:
+        y : array_like
+
+        """
+        return self._value
+
+    def reverse(self):  # return the derivative with respect to varname variable
+        """Get the derivatives (scalar, vector, or matrix depending on the
+        the dimension of input method fn and X)
+
+        Returns:
+        y : array_like
+
+        """
+        return self._der
--- tests/test_driver.py
+++ tests/test_driver.py
@@ -48,3 +48,132 @@ def test_forward():
     except AssertionError as e:
         print(e)
         raise AssertionError
+
+
+def test_get_forwardpass1f():
+    """Test of _forwardpass1f method, i.e., testing one-function situation"""
+    def f(x):
+        return Elem.sin(x)
+    function = ad.RAutoDiff(f)
+
+    # test scalar input
+    function.forwardpass(1.0)
+    try:
+        assert function.values() == np.sin(1.0)
+        assert function.reverse() == np.cos(1.0)
+    except AssertionError as e:
+        print(e)
+        raise AssertionError
+
+    # test fi(x) at different x values
+    function.forwardpass([1.0,2.0])
+    try:
+        assert np.array_equal(function.values(), np.sin([1.0,2.0]))
+        assert np.array_equal(function.reverse(), np.cos([1.0,2.0]))
+
+    except AssertionError as e:
+        print(e)
+        raise AssertionError
+
+    # test error due to given 2D input to fi(x)
+
+    with pytest.raises(TypeError) as excinfo:
+        function.forwardpass([[1.0,2.0],[1.0,2.0]])
+    assert "input dimension size not supported" in str(excinfo.value)
+
+    def f2d(x, y):
+        return x * y
+
+    function = ad.RAutoDiff(f2d)
+    # test f2d(x,y) at point (x,y)
+    function.forwardpass([1,2])
+    try:
+        assert function.values() == 2.
+        assert np.array_equal(function.reverse(), [2,1])
+    except AssertionError as e:
+        print(e)
+        raise AssertionError
+
+    with pytest.raises(TypeError) as excinfo:
+        function.forwardpass(1)
+    assert "input has insufficient parameters" in str(excinfo.value)
+
+    with pytest.raises(TypeError) as excinfo:
+        function.forwardpass([1,2,3,4])
+    assert "input dimension size mismatch" in str(excinfo.value)
+
+    with pytest.raises(TypeError) as excinfo:
+        function.forwardpass([[1,2,3,4],[1,2,3,4]])
+    assert "input dimension size mismatch" in str(excinfo.value)
+
+    # test f2d(x,y) at multiple points, i.e., (x,y) and (x1, y1)
+    function.forwardpass([[1,2],[3,4]])
+    try:
+        assert np.array_equal(function.values(), [2,12])
+        assert np.array_equal(function.reverse(), [[2,1],[4,3]])
+    except AssertionError as e:
+        print(e)
+        raise AssertionError
+
+
+def test_get_forwardpass():
+    """Test of _forwardpass method, i.e., testing vector function situation"""
+    def f1(x):
+        return Elem.sin(x)
+
+    def f2(x):
+        return Elem.cos(x)
+
+    function = ad.RAutoDiff([f1, f2])
+    # test scalar input
+    function.forwardpass(1.0)
+    try:
+        assert np.array_equal(function.values(), [np.sin(1), np.cos(1)])
+        assert np.array_equal(function.reverse(), [np.cos(1), -np.sin(1)])
+    except AssertionError as e:
+        print(e)
+        raise AssertionError
+
+    # test [f1(x),f2(x)] at different x values
+    function.forwardpass([1,2])
+    try:
+        assert np.array_equal(function.values(), [[np.sin(1), np.cos(1)],[np.sin(2), np.cos(2)]])
+        assert np.array_equal(function.reverse(), [[np.cos(1), -np.sin(1)],[np.cos(2), -np.sin(2)]])
+    except AssertionError as e:
+        print(e)
+        raise AssertionError
+
+    def f2d1(x, y):
+        return x * y
+
+    def f2d2(x, y):
+        return 2 * x * y
+
+    function = ad.RAutoDiff([f2d1, f2d2])
+    # test [f2d1(x,y), f2d1(x,y)] at point (x,y)
+    function.forwardpass([1,2])
+    try:
+        assert np.array_equal(function.values(),[2,4])
+        assert np.array_equal(function.reverse(), [[2,1],[4,2]])
+    except AssertionError as e:
+        print(e)
+        raise AssertionError
+
+    # test [f2d1(x,y), f2d1(x,y)] at multiple points, i.e., (x,y) and (x1, y1)
+
+    function.forwardpass([[1,2],[3,4]])
+    try:
+        assert np.array_equal(function.values(),[[2,4],[12,24]])
+        assert np.array_equal(function.reverse(), [[[2,1],[4,2]],[[4,3],[8,6]]])
+    except AssertionError as e:
+        print(e)
+        raise AssertionError
+
+    def f2d3(x):
+        return Elem.sin(x)
+
+    with pytest.raises(TypeError) as excinfo:
+        function = ad.RAutoDiff([f2d2, f2d3])
+        function.forwardpass([1,2])
+    assert "all input functions must contain the same parameters" in str(excinfo.value)
+    function = ad.RAutoDiff([f2d2, f2d3])
